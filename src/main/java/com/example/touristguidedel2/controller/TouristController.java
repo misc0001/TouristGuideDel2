652fa1f316551b4fcdcddcedef92e12f4c0ea7ee
Oskar <osth0002@stud.kea.dk>
2024-02-19 11:24:14 +0100

    Update branch 1

--- src/main/java/com/example/touristguidedel2/controller/TouristController.java
+++ src/main/java/com/example/touristguidedel2/controller/TouristController.java
@@ -1,9 +1,13 @@
 package com.example.touristguidedel2.controller;
 
+import com.example.touristguidedel2.model.TouristAttraction;
 import com.example.touristguidedel2.service.TouristService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,4 +24,12 @@ public class TouristController {
         model.addAttribute("tourist", touristService.getAllAttractions());
         return "attraction-index";
     }
+
+    @PostMapping("update")
+    public void updateAttraction(@RequestBody TouristAttraction touristAttraction) {
+
+    }
+
+
+
 }

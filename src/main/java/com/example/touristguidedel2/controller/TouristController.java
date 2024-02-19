75fee6df2dbd75b3e366458dfab047eb2303cebf
misc0001 <misc0001@stud.kea.dk>
2024-02-19 18:34:51 +0100

    igang med Add

--- src/main/java/com/example/touristguidedel2/controller/TouristController.java
+++ src/main/java/com/example/touristguidedel2/controller/TouristController.java
@@ -1,9 +1,12 @@
 package com.example.touristguidedel2.controller;
 
+import com.example.touristguidedel2.model.TouristAttraction;
 import com.example.touristguidedel2.service.TouristService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,16 @@ public class TouristController {
     public String showAttractions(Model model) {
         model.addAttribute("tourist", touristService.getAllAttractions());
         return "attraction-index";
+    }
+    @GetMapping("add")
+    public String showAddedAttraction(Model model) {
+        TouristAttraction newAttraction = new TouristAttraction();
+        model.addAttribute("attraction", newAttraction);
+        return "attraction-add";
+    }
+    @PostMapping("add")
+    public String addAttraction(@ModelAttribute TouristAttraction touristAttraction) {
+        touristService.addAttraction(touristAttraction);
+        return "redirect/attractions";
     }
 }

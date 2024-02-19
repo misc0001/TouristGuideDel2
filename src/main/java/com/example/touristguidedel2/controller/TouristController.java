4389cfcc227b8ac0f67be1f378451b3fbbad03b4
misc0001 <misc0001@stud.kea.dk>
2024-02-19 19:04:25 +0100

    Add fungere nu :P

--- src/main/java/com/example/touristguidedel2/controller/TouristController.java
+++ src/main/java/com/example/touristguidedel2/controller/TouristController.java
@@ -32,6 +32,6 @@ public class TouristController {
     @PostMapping("add")
     public String addAttraction(@ModelAttribute TouristAttraction touristAttraction) {
         touristService.addAttraction(touristAttraction);
-        return "redirect/attractions";
+        return "redirect:/attractions";
     }
 }

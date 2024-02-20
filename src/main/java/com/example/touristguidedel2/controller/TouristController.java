1501d7a5edb5ee2cbc7489f1e92b35e7d66c9b2b
Oskar <osth0002@stud.kea.dk>
2024-02-20 10:42:23 +0100

    Delete Branch 1

--- src/main/java/com/example/touristguidedel2/controller/TouristController.java
+++ src/main/java/com/example/touristguidedel2/controller/TouristController.java
@@ -2,13 +2,11 @@ package com.example.touristguidedel2.controller;
 
 import com.example.touristguidedel2.model.TouristAttraction;
 import com.example.touristguidedel2.service.TouristService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("attractions")
@@ -25,11 +23,14 @@ public class TouristController {
         return "attraction-index";
     }
 
-    @PostMapping("update")
-    public void updateAttraction(@RequestBody TouristAttraction touristAttraction) {
+    @DeleteMapping("{id}/delete")
+    public ResponseEntity<TouristAttraction> deleteAttraction(@PathVariable int id) {
+        TouristAttraction deleteAttraction = touristService.deleteAttraction(id);
 
+        if (deleteAttraction != null) {
+            return new ResponseEntity<>(deleteAttraction, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-
-
-
 }
